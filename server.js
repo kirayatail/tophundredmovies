6a2a93d3bd0b4191ff1b39039671ec4fff1d8c66
Max Witt <kirayatail@gmail.com>
2021-04-20 21:14:50 +0200

    cleanup, readme, local dev fixes

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const tmdb = require('tmdbv3').init(process.env.TMDB_API_KEY);
 const app = express();
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('build'));
 
 app.get('/api/movies', (req, res) => {
   Promise.all(([1,2,3,4,5]).map(popularPromise)).then(pages => {
+    // Yeah... This is super fast, so I added some Zen
     setTimeout(() => {
       res.send(pages.map(page => page.results).flat());
     }, 3000);
